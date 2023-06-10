23395adb087742814630ab32509178d4d565d51c
rosshjb <jinbeomhong@gmail.com>
2023-06-10 20:07:02 +0900

    1장 끝

--- oracle/친절한 SQL 튜닝/1장 - SQL 처리 과정과 IO.sql
+++ oracle/친절한 SQL 튜닝/1장 - SQL 처리 과정과 IO.sql
@@ -52,3 +52,79 @@ set autotrace off;
 --------------------------------------------------------------------------------
 -- 1.1.5 옵티마이저 힌트
 --------------------------------------------------------------------------------
+create table 고객(
+    고객ID varchar2(10) constraint 고객_pk primary key,
+    고객명 varchar2(10) not null,
+    연락처 varchar2(20) not null,
+    주소 varchar2(100)  not null,
+    가입일시 timestamp  default systimestamp not null
+);
+
+select /*+ index(a 고객_pk) */
+    고객명, 연락처, 주소, 가입일시
+from 고객 a
+where 고객id = '000000008';
+
+select --+ index(a 고객_pk)
+    고객명, 연락처, 주소, 가입일시
+from 고객 a
+where 고객id = '000000008';
+
+drop table 고객;
+--------------------------------------------------------------------------------
+-- 주의사항
+--------------------------------------------------------------------------------
+select /*+ index(a a_x01) index(b b_x03) */
+    a.dummy a_dummy, b.dummy b_dummy
+from dual a, dual b;
+
+select /*+ index(c), full(d) */
+    c.dummy c_dummy, d.dummy d_dummy
+from dual c, dual d;
+
+select /*+ full(scott.emp) */ *
+from emp;
+
+select /*+ full(emp) */ *
+from emp e;
+
+select * from table(dbms_xplan.display_cursor(null, 0, 'advanced allstats last'));
+--------------------------------------------------------------------------------
+-- 자율이냐 강제냐, 그것이 문제
+--------------------------------------------------------------------------------
+create table 고객(
+    고객ID varchar2(10) constraint 고객_pk primary key,
+    고객명 varchar2(10) not null,
+    연락처 varchar2(20) not null,
+    주소 varchar2(100)  not null,
+    가입일시 timestamp  default systimestamp not null
+);
+
+create table 주문(
+    주문번호 number(10),
+    주문일자 date default sysdate not null,
+    주문금액 number(10) not null,
+    고객id varchar2(10) not null,
+    constraint 주문번호_pk primary key(주문번호),
+    constraint 고객id_fk foreign key(고객id) references 고객(고객id)
+);
+
+create index 주문_주문일자_고객id_idx on 주문(주문일자, 고객id);
+
+select /*+ index(a (주문일자)) */
+    a.주문번호, a.주문금액, b.고객명, b.연락처, b.주소
+from 주문 a, 고객 b
+where a.주문일자 = :ord_dt
+and a.고객id = b.고객id;
+
+select /*+ leading(a) use_nl(b) index(a (주문일자)) index(b 고객_pk) */
+    a.주문번호, a.주문금액, b.고객명, b.연락처, b.주소
+from 주문 a, 고객 b
+where a.주문일자 = :ord_dt
+and a.고객id = b.고객id;
+
+drop table 주문;
+drop table 고객;
+--------------------------------------------------------------------------------
+-- 1.2 SQL 공유 및 재사용
+--------------------------------------------------------------------------------
